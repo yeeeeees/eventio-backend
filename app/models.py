9cbbb2b5c06d4ada1ea5a178c3601b7cfaff0622
isakal <sakyYT@gmail.com>
2020-01-16 23:18:45 +0100

    Add many-many user-event relation, joining events is now possible - added association table for relationship - joining events now possible # TODO add joining and leaving mutation - added location column in event table - renamed few columns for better clarity

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 
 def datetime_now_iso_format():
-    return datetime.now().isoformat()
+    return datetime.now().isoformat()[:-10]
 
 
 class User(db.Model):
@@ -15,7 +15,8 @@ class User(db.Model):
     profile_pic = db.Column(db.Text(), default="default.jpg")
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(50), nullable=False)
-    events = db.relationship("Event", backref="organizer", lazy=True)
+    created_events = db.relationship("Event", backref="organizer", lazy=True)
+    joined_events = db.relationship("Event", secondary="joins", backref="joined_users")
 
     def __repr__(self):
         return f"User({self.uuid}, {self.fname}, {self.surname}, {self.email})"
@@ -24,9 +25,16 @@ class User(db.Model):
 class Event(db.Model):
     uuid = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120), nullable=False)
-    date_posted = db.Column(db.String(50), nullable=False, default=datetime_now_iso_format)
+    date_posted = db.Column(db.String(25), nullable=False, default=datetime_now_iso_format)
+    location = db.Column(db.String(100))
     description = db.Column(db.Text)
-    user_uuid = db.Column(db.Integer, db.ForeignKey("user.uuid"), nullable=False)
+    organizer_uuid = db.Column(db.Integer, db.ForeignKey("user.uuid"), nullable=False)
 
     def __repr__(self):
-        return f"Event({self.uuid}, {self.title}, {self.description}, oranizer id={self.user_uuid})"
+        return f"Event({self.uuid}, {self.title}, {self.description}, oranizer id={self.organizer_uuid})"
+
+
+joins = db.Table("joins",
+                 db.Column("user_uuid", db.Integer, db.ForeignKey("user.uuid")),
+                 db.Column("event_uuid", db.Integer, db.ForeignKey("event.uuid"))
+                 )
